fdfc7de8eae28b16dec775b4e587c78fce0aa7f1
afran <number1qe@gmail.com>
2022-08-02 16:37:41 -0400

    testerz1

---
+++ src/First_package/First_class.java
@@ -0,0 +1,10 @@
+package First_package;
+
+public class First_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/SecondPackage/thirclass.java
@@ -0,0 +1,10 @@
+package SecondPackage;
+
+public class thirclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
